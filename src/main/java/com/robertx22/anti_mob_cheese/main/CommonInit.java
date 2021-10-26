ec727e411e1f9d23ebdb2ae31e7f764956a3612c
robert trebor <treborx555@gmail.com>
2021-10-26 20:20:10 +0200

    1.2.1 configs

--- src/main/java/com/robertx22/anti_mob_cheese/main/CommonInit.java
+++ src/main/java/com/robertx22/anti_mob_cheese/main/CommonInit.java
@@ -14,7 +14,9 @@ import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -24,6 +26,9 @@ public class CommonInit {
     public CommonInit() {
         final IEventBus bus = FMLJavaModLoadingContext.get()
             .getModEventBus();
+
+        ModLoadingContext.get()
+            .registerConfig(ModConfig.Type.COMMON, CheeseConfig.commonSpec);
 
         bus.addListener(this::commonSetupEvent);
 
